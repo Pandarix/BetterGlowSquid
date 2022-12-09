bbdd86807fa7fb5512348a2278e67ac3a57d4877
Ruven <ruven@uni-bremen.de>
2022-12-09 20:56:11 +0100

    added model files

--- src/main/java/net/Pandarix/betterglowsquid/init/ItemInit.java
+++ src/main/java/net/Pandarix/betterglowsquid/init/ItemInit.java
@@ -1,6 +1,12 @@
 package net.Pandarix.betterglowsquid.init;
 
 import net.Pandarix.betterglowsquid.BetterGlowSquid;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public final class ItemInit {
     private ItemInit() {
